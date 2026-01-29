6e355d03d7b0b1236a51517762eee8c8e5b160b5
suzan hamami <suzanhamami3@gmail.com>
2026-01-29 18:43:09 +0300

    handling get requests

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const app = express();
 
+const courses =[
+    { id: 1, name: 'course1 '},
+    { id: 2, name: 'course2 '},
+    { id: 3, name: 'course3 '},
+];
+
 app.get('/', (req,res) => {
     res.send('Hello World!!!');
 });
 
 app.get('/api/courses', (req,res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req,res) => {
-    res.send(req.params.id);
-})
-//we can have multiple parameters:
-app.get('/api/posts/:year/:month', (req,res) => {
-    res.send(req.params);
-})
-//for query: like ?sortBy=name
-app.get('/api/posts/:year/:month', (req,res) => {
-    res.send(req.query);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) 
+        res.status(404).send('The course was not found');
+    else
+        res.send(course);
 })
 
 //PORT env var
 process.env.PORT=5000;
 //! i should set the port by set port=5000 but its not working in the terminal so i did it manually
 const port = process.env.PORT || 3000;
-console.log(process.env.port);
 app.listen(port, () => console.log(`Listening on port ${port}`));
