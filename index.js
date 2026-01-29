f6fb500ebc93b96d96c0a6fff199894ed377caa6
suzan hamami <suzanhamami3@gmail.com>
2026-01-29 03:49:59 +0300

    added nodemon package, and learned about port env var

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send('Hello World');
+    res.send('Hello World!!!');
 });
 
 app.get('/api/courses', (req,res) => {
     res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+//PORT env var
+process.env.PORT=5000;
+//! i should set the port by set port=5000 but its not working in the terminal so i did it manually
+const port = process.env.PORT || 3000;
+console.log(process.env.port);
+app.listen(port, () => console.log(`Listening on port ${port}`));
