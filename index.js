ec41843376b377b51a2e190a596e1cc55d432c09
suzan hamami <suzanhamami3@gmail.com>
2026-03-26 21:23:37 +0300

    minor add

--- index.js
+++ index.js
@@ -41,6 +41,10 @@ app.use(express.json());
 
 app.use('/api/courses', coursesRoutes);
 
+app.get('/', (req, res) => {
+    res.send('API is running 🚀');
+});
+
 app.listen(port, () =>
     console.log(`Server running at http://localhost:${port}`)
 );
